4ed627a075d230cd53003bb88b3d62d17b0808f6
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-06-10 07:45:23 +0530

    Add files via upload

---
+++ Data/StudentDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentCrudApiWithEFCore.Models;
+
+namespace StudentCrudApiWithEFCore.Data
+{
+    public class StudentDbContext:DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options) { }
+
+        public DbSet<Student> StudentInfo { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCrudApiWithEFCore.Models
+{
+    public class Student
+    {
+        [Key]
+        public int studentId { get; set; }
+
+        [Required]
+        public string studentName { get; set; }
+
+        [Required]
+        public string studentDepartment { get; set; }
+    }
+}
